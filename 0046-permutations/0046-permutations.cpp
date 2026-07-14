7e5bf9dc04e712eec17dafa874c1df7561209a9d
ANKIT <166922880+ankit1207yadav@users.noreply.github.com>
2026-07-14 18:55:47 +0530

    Time: 0 ms (100%), Space: 10.5 MB (71.46%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    void backtrack(vector<int> &nums, vector<bool> &used, vector<int> &current, vector<vector<int>> &result){
+        // base case jab fat k haat mei ho
+        if (current.size()==nums.size()){
+            result.push_back(current);
+            return;
+        }
+        for (int i=0; i<nums.size(); i++){
+            if (used[i]) continue; // skip agar hai toh
+
+            used[i] = true;  // include kro
+            current.push_back(nums[i]);
+
+            backtrack(nums, used, current, result);
+            current.pop_back();
+            used[i] = false;
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+         vector <vector<int>> result;
+         vector <int> current;
+         vector <bool> used(nums.size(), false);
+
+         backtrack(nums, used, current, result);
+         return result; 
+    }
+};
