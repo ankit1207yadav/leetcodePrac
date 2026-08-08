e299ab23aa3b4a83c376dbfd2ba71d725102a66c
ANKIT <166922880+ankit1207yadav@users.noreply.github.com>
2026-08-08 11:07:28 +0530

    Time: 0 ms (100%), Space: 9.9 MB (9.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack <char> st;
-        for (char ch : s){
-            if (ch =='(' || ch =='[' || ch=='{'){
-                st.push(ch);
-            } else {
-                if (st.empty()){
-                    return false;
-                } 
-                if (ch == ')' && st.top()!='(' || ch == ']' && st.top()!='[' || ch == '}' && st.top()!='{'){
-                    return false;
-                }
-                st.pop();
-            }
+     stack <char> st;
+     for (char &ch : s) {
+        if (ch == '('){
+            st.push(')');
         }
-        
-        return st.empty();
+        else if (ch == '{'){
+            st.push('}');
+        }
+        else if (ch == '['){
+            st.push(']');
+        } else if (st.empty() || ch != st.top()){
+            return false;
+        } else {
+            st.pop();
+        }
+     } 
+     return st.empty(); 
     }
 };
